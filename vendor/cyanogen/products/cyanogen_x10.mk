85c213ad22f6e31a157039849fbf937601d92f34
Jerpelea <jerpelea@gmail.com>
2011-03-12 07:49:24 +0200

    added gb keys for market

--- vendor/cyanogen/products/cyanogen_x10.mk
+++ vendor/cyanogen/products/cyanogen_x10.mk
@@ -15,7 +15,8 @@ PRODUCT_BRAND := SonyEricsson
 PRODUCT_DEVICE := x10
 PRODUCT_MODEL := X10
 PRODUCT_MANUFACTURER := SonyEricsson
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SonyEricsson_x10 BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SonyEricsson_x10 BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/se/x10/prelink-linux-arm-x10.map
 
@@ -31,7 +32,7 @@ else
             ro.modversion=CyanogenMod-7.0.0-RC1-X10
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-RC1-X10-J006
+            ro.modversion=CyanogenMod-7.0.0-RC2-X10-J009
     endif
 endif
 
--- vendor/cyanogen/products/cyanogen_x8.mk
+++ vendor/cyanogen/products/cyanogen_x8.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
+# Include FM stuff
+$(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
+
 #
 # Setup device specific product configuration.
 #
@@ -15,7 +18,8 @@ PRODUCT_BRAND := SonyEricsson
 PRODUCT_DEVICE := x8
 PRODUCT_MODEL := X8
 PRODUCT_MANUFACTURER := SonyEricsson
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SonyEricsson_x8 BUILD_ID=FRG83 BUILD_DISPLAY_ID=GRH78C BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.2.1/FRG83/60505:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.2.1 FRG83 60505 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SonyEricsson_x8 BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
+
 
 PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/device/se/x8/prelink-linux-arm-x8.map
 
@@ -28,10 +32,10 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-RC1-x8
+            ro.modversion=CyanogenMod-7.0.0-RC2-x8
     else
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.0.0-RC1-x8-J003
+            ro.modversion=CyanogenMod-7.0.0-RC2-x8-J005
     endif
 endif
 
