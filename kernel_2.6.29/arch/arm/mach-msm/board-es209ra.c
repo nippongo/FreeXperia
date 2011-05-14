691809bbadf35ed22ac2be390e7113301937b6a5
Jerpelea <jerpelea@gmail.com>
2011-05-14 10:54:29 +0300

    2.6.30 kernel fix

--- kernel_2.6.29/arch/arm/mach-msm/board-es209ra.c
+++ kernel_2.6.29/arch/arm/mach-msm/board-es209ra.c
@@ -1076,14 +1076,14 @@ static void __init msm_mddi_tmd_fwvga_display_device_init(void)
 
 	panel_data->panel_info.mddi.vdopkt = MDDI_DEFAULT_PRIM_PIX_ATTR;
 
-	panel_data->panel_info.lcd.vsync_enable = TRUE;
+	panel_data->panel_info.lcd.vsync_enable = FALSE;
 	panel_data->panel_info.lcd.v_back_porch = 12;
 	panel_data->panel_info.lcd.v_front_porch = 2;
 	panel_data->panel_info.lcd.v_pulse_width = 0;
-	panel_data->panel_info.lcd.hw_vsync_mode = TRUE;
+	panel_data->panel_info.lcd.hw_vsync_mode = FALSE;
 	panel_data->panel_info.lcd.vsync_notifier_period = 0;
 
-	panel_data->panel_info.lcd.refx100 = 200000000 / 16766;
+	panel_data->panel_info.lcd.refx100 = 200000000 / 13389;
 
 	panel_data->panel_ext = &tmd_wvga_panel_ext;
 
